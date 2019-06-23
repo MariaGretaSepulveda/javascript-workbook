4b1c2597e63b0e5c0eab1710ff2815337b0abd9a
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-06-23 16:55:56 -0500

    Pokemon Changes

--- 08week/APITest/apitest.js
+++ 08week/APITest/apitest.js
@@ -1,44 +1,99 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
+let p1 = null;
+let p1name = null;
+let p1value = null;
+let p2 = null;
+let p2name = null;
+let p2value = null;
 
-
-const getAddress = (fetch, id) =>{
-   return fetch ('https://randomuser.me/api/'+id+ '?key='+key);
-
+let getPokemon1 = () => {
+  fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(function (response) {
+      return response.json() //gets the pokemon object and api
+    })
+    .then(function (data) {
+      let random = Math.floor(Math.random() * 20)
+      console.log('random: ', random)
+      let player = data.results[random]
+      p1 = player //the object
+      return fetch(player.url) //access that api within that pokemon
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // getting pokemon name here!
+      p1value = data.base_experience
+      let p1name = data.species.name
+        let display = `<h1> ${p1name} </h1>`
+        display += 
+    // Experience: ${p1value}
+        `
+        <br>
+        <img src="   ${data.sprites.front_shiny}" >
+        `
+        document.getElementById('pokeName1').innerHTML = display;
+    })
+    .catch(function (error) {
+      console.log('Requestfailed', error)
+    });
 }
-//Test
-if (typeof describe === 'function') {
-//miss typed address
-    describe('Test Fetch', () => {
-        let count =0;
 
-        const fakeFetch =(url) => 
-        {
-            const arr=url.split('/');
-            let id=Number(arr[arr.length-1]);
-        if(id <= 0 || id > 30)
-            return 'Out of range'
-        else
-            return url;
-        }
-      it('check for miss typed address', () => {
-        
-        assert.equal(getAddress(fakeFetch,15), 'https://randomuser.me/api/15');
-    });
-    it('Out of range', () => {
-        
-        assert.equal(getAddress(fakeFetch,31), 'Out of range');
-    });
-    //Pick up Here! Not complete!
-    it('Wrong API Keys', () => {
-        
-        assert.equal(getAddress(fakeFetch,31), 'Wrong API Key');
-    });
-})
-}else {
-    console.log('Only run this test')
-}
-//out of range request 
+let getPokemon2 = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon`)
+      .then(function (response) {
+        return response.json()//getis the pokemon object and api
+      })
+      .then(function (data) {
+        let random = Math.floor(Math.random() * 20)
+        console.log('random: ', random)
+        let player = data.results[random]
+        p2 = player //the object
+        return fetch(player.url) //access that api within that pokemon
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // getting pokemon name here!
+        p2value = data.base_experience
+        let p2name = data.species.name
+        let display = `<h1> ${p2name} </h1>`
+        display += 
+        // Experience: ${p2value}
+        `
+        <br>
+        <img src="${data.sprites.front_shiny}" >
+        `
+        document.getElementById('pokeName2').innerHTML = display;
+      })
+      .catch(function (error) {
+        console.log('Requestfailed', error)
+      });
+  }
 
-//wrong API key 
+  let checkForWin = ()=> {
+      if(p1 !== null && p2 !== null){
+          console.log('working here')
+          console.log(p1value, p2value)
+          console.log(p1.name, p2.name)
+        if(p1value === p2value){
+            console.log(`It's a draw!`)
+            document.getElementById('announcement').innerHTML = `It's a Draw!`
+          } else if(p2value > p1value){
+            console.log(`Winner: ${p2.name}!`)
+            document.getElementById('announcement').innerHTML = `${p2.name} wins!`
+          } else if(p1value > p2value){
+            console.log(`Winner: ${p1.name}!`)
+            document.getElementById('announcement').innerHTML = `${p1.name} wins!`
+
+          }
+      }
+  }
+
+  let restart = () => {
+    window.location.href=window.location.href
+  }
+
+
